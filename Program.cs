1da160e9959df9cfb6e295aa7f6f0c7337ad11c7
kolia <200drexc@gmail.com>
2020-09-20 18:56:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+enum Genders
+{
+    Male,
+    Female
+}
+struct Person
+{
+    public string firstName;
+    public string LastName;
+    public int age;
+    public Genders genders;
+
+    public Person(String firstName, string LastName, int age, Genders genders)
+    {
+        this.firstName = firstName;
+        this.LastName = LastName;
+        this.age = age;
+        this.genders = genders;
+    }
+
+    public override string ToString()
+    {
+        return firstName + " " + LastName + "(" + genders + "), age" + age;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("Tony", "Allen", 20, Genders.Male);
+        Console.WriteLine(person.ToString());
+    }
+}
